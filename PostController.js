5d1e01571bb02098146cfbfe4075a04c78a4171e
Archie <tomcher@yandex.com>
2025-02-10 00:29:03 +0300

    CRUD realized

--- PostController.js
+++ PostController.js
@@ -11,22 +11,56 @@ class PostController {
     }
   }
 
-async getAll(req, res) {
+  async getAll(req, res) {
+    try {
+      const posts = await Post.find();
+      return res.json(posts);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
 
-}
+  async getOne(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: "Id не указан" });
+      }
+      const post = await Post.findById(id);
+      return res.json(post);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
 
-async getOne(req, res) {
+  async update(req, res) {
+    try {
+      const post = req.body;
+      if (!post._id) {
+        res.status(400).json({ message: "Id не указан" });
+      }
+      const updatedPost = await Post.findByIdAndUpdate(post._id, post, {
+        new: true,
+      });
+      return res.json(updatedPost);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
 
-}
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: "Id не указан" });
+      }
 
-async update(req, res) {
-
-}
-
-async delete(req, res) {
-
-}
-
+      const post = await Post.findByIdAndDelete(id);
+      return res.json(post);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
 }
 
 export default new PostController();
--- router.js
+++ router.js
@@ -6,7 +6,7 @@ const router = new Router();
 
 router.post("/posts", PostController.create);
 router.get("/posts", PostController.getAll);
-router.get("posts/:id", PostController.getOne);
+router.get("/posts/:id", PostController.getOne);
 router.put("/posts", PostController.update);
 router.delete("/posts/:id", PostController.delete);
 
