77a3d7268f9e370789fdf9a592719bcf057264f0
Archie <tomcher@yandex.com>
2025-02-09 23:40:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from "./router.js";
+
+const PORT = 5002;
+const DB_URL = `mongodb+srv://user:user@cluster0.wol1i.mongodb.net/`;
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", router);
+
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+startApp();
